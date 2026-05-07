9bc4cccff6d4a91ae6b43e8638116d2694a084e1
Matthew <vanschoormatthew@gmail.com>
2026-05-07 13:22:40 +0200

    Fixed an issue with components scrolling independantly on mobile devices. Removed the second picture of me in the about section, and cleaned up every css file for clarity purposes.

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,23 +6,21 @@ import project3 from './RIADP.jpg';
 
 function Projects() {
     const projectList = [
-        {title: "Zenbuilder", image: project1, desc: "A Zenless Zone Zero companion application designed to help players optimize their accounts."},
-        {title: "ValoMeta", image: project2, desc: "A website dedicated to providing meta-game data for Valorant players."},
-        {title: "RIADP", image: project3, desc: "An application used to identify risk zones with an overlay for predicted drought models."}
+        { title: "Zenbuilder", image: project1, desc: "A Zenless Zone Zero companion application designed to help players optimize their accounts." },
+        { title: "ValoMeta", image: project2, desc: "A website dedicated to providing meta-game data for Valorant players." },
+        { title: "RIADP", image: project3, desc: "An application used to identify risk zones with an overlay for predicted drought models." }
     ];
 
     return (
         <section className="projectSection" id="projects">
-            <div className="projectHeading">
-                Project <br/>Showcase
-            </div>
+            <h1 className="projectHeading">Project <br/>Showcase</h1>
 
             <div className="projectGrid">
                 {projectList.map((item, index) => (
                     <div className="projectCard" key={index}>
                         <h2 className="projectName">{item.title}</h2>
                         <p className="projectDesc">{item.desc}</p>
-                        <div className="projectImgContainer">
+                        <div className="projectImgWrap">
                             <img src={item.image} className="projectImg" alt={item.title}/>
                         </div>
                     </div>
